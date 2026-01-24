72705db311fe266223da3b3ebc530f56bbba4999
theHrDev <hamzatrukayat2822@gmail.com>
2026-01-24 09:32:34 +0000

    Updated the tooplate script

--- tooplate.sh
+++ tooplate.sh
@@ -6,8 +6,14 @@ wget https://www.tooplate.com/zip-templates/2099_scenic.zip
 sudo dnf install unzip -y
 unzip 2099_scenic.zip -d template
 
+
 sudo rm -rf /var/www/html/*
-sudo mv template/* /var/www/html
+
+sudo dnf install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+sudo mv template/2099_scenic/* /var/www/html
 
 echo "Successful"
 
