5a06e1b62d39a06b19947f21eaa46b3fc570542b
theHrDev <hamzatrukayat2822@gmail.com>
2026-01-26 20:59:25 +0000

    Added the prompt to enter branch name

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 read -p "Enter your commit message: " message
+read -p "Enter your branch: " branch
 sudo dnf install git
 git add $1
 git commit -m "$message"
-git push origin main
+git push origin "$branch"
 
