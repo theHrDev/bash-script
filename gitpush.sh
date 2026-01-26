ae2f4e2db567166a708015828d1342be5128b60e
theHrDev <hamzatrukayat2822@gmail.com>
2026-01-26 21:05:19 +0000

    Added the check function

--- gitpush.sh
+++ gitpush.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 read -p "Enter your commit message: " message
 read -p "Enter your branch: " branch
-sudo dnf install git
-git add $1
-git commit -m "$message"
-git push origin "$branch"
+sudo dnf install git > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+	echo "git successfully installed"
+	git add $1
+
+        git commit -m "$message"
+        git push origin "$branch"
+
+	echo "successfully pushed to branch $branch"
+
+else
+	echo "Error occured while installing git"
+
+fi
 
