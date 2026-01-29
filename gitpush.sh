0e4b98f8ef0b65576fe250eae7be7bf32d77e64b
theHrDev <hamzatrukayat2822@gmail.com>
2026-01-29 18:14:31 +0000

    Updated the git push script

--- gitpush.sh
+++ gitpush.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 read -p "Enter your commit message: " message
 read -p "Enter your branch: " branch
-sudo dnf install git > /dev/null 2>&1
 
-if [ $? -eq 0 ]; then
-	echo "git successfully installed"
-	git add $1
 
-        git commit -m "$message"
-        git push origin "$branch"
+if command -v git > /dev/null 2>&1; then
 
-	echo "successfully pushed to branch $branch"
+	echo "git has already been installed"
 
 else
-	echo "Error occured while installing git"
+	echo "git not found installing..."
+	sudo dnf install git > /dev/null 2>&1
+
+	if [ $? -eq 0 ]; then
+		echo "Successfully installed git, proceesing to pushing..."
+	else
+
+		echo "Encountered error while intalling git"
+		exit 1
+	fi
+fi
+
+# Git flow
+git add $1
+git commit -m "$message"
+git push origin "$branch"
+
+if [$? -eq 0 ]; then
+	echo "Successfully push to branch $branch "
+else
+	echo "Error pushing to github"
 
 fi
+
+
 
